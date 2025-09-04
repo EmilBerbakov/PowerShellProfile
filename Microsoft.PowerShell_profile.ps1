817513220a2834d07e9159c58a74e39a04e581ca
Emil Berbakov <emberbakov@gmail.com>
2025-09-03 22:40:56 -0400

    some edits

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,26 +1,37 @@
 fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
 fnm use 22 >$null
 
-function Get-GitBranch {
-  return git branch --show-current
-}
-function prompt {
-  $gitbranch = Get-GitBranch
-  if ($gitbranch) {
-    $color = switch ($gitbranch) {
-	{($_ -eq 'main') -or ($_ -eq 'master')} { "Red" }
-	{($_ -eq 'develop')} { "Green" }
-	default { "Blue" }
-    }
-    Write-Host "$($executionContext.SessionState.Path.CurrentLocation)" -ForegroundColor DarkMagenta -NoNewLine 
-    Write-Host " ($gitbranch)" -ForegroundColor $color -NoNewLine
-    Write-Host "$('>'*($NestedPromptLevel + 1))" -NoNewLine
-    return ' '
-  }
-  else {
-    Write-Host "$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($NestedPromptLevel + 1)) " -Foreground DarkMagenta -NoNewLine
-    return ' '
-  }
+function Get-GitBranch
+{
+	return git branch --show-current
 }
 
-$nvim = $env:localappdata + '\nvim'
+function prompt
+{
+	$gitbranch = Get-GitBranch
+	Write-Host "$($executionContext.SessionState.Path.CurrentLocation)" -NoNewLine 
+
+	if ($gitbranch)
+	{
+		$color = switch ($gitbranch)
+		{
+			{($_ -eq 'main') -or ($_ -eq 'master')}
+			{ "Red" 
+			}
+			{($_ -eq 'develop')}
+			{ "Green" 
+			}
+			default
+			{ "Blue" 
+			}
+		}
+		Write-Host "($gitbranch)" -ForegroundColor $color -NoNewLine
+	}
+
+	Write-Host "$('>'*($NestedPromptLevel + 1))" -ForegroundColor Green -NoNewLine
+	return ' '
+}
+
+$NVIM = $env:localappdata + '\nvim'
+$NVIM_INIT = $NVIM + '\init.lua'
+$DEV = '~\Documents'
